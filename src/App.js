8c621c2ac1b836e399ea310dada89b09872236d9
JJ <153640426+KodeckJames@users.noreply.github.com>
2025-03-20 20:41:49 +0300

    UseState hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import react, { useState } from 'react';
+
 
 function App() {
+  const [count, setCount] = useState(4);
+  const [theme, setTheme] = useState('Purple');
+  const add=()=>{
+    setCount(prevcount => prevcount + 1)
+  }
+  const subtract=()=>{
+    setCount(prevcount => prevcount - 1)
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex flex-1 justify-center items-center h-screen'>
+      <button className='bg-blue-500 p-4 rounded text-white' onClick={add}>+</button>
+      <span className=' p-2 font-bold text-xl'>{count}</span>
+      <span className=' p-4'>{theme}</span>
+      <button className='bg-blue-500 p-4 rounded text-white' onClick={subtract}>-</button>
     </div>
   );
 }
